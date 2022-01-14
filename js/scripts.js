9e0945ff01a9a4ed37928b694dc26a8c8a382ef3
jakechaley <jakechaley@gmail.com>
2022-01-14 10:35:33 -0800

    continue adding javaScript to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
+const seasonScores = {
+  autumn: 20,
+  winter: 15,
+  summer: 45,
+  spring: 10
+}
+// console.log(seasonScores)
+// console.log(seasonScores.autumn)
+
+const weekDayScores = {
+  monday: 25,
+  tuesday: 55,
+  wednesday: 5,
+  thursday: 60,
+  friday: 95,
+  saturday: 50,
+  sunday: 80  
+}
+
+const catsOrDogsScores = {
+  cats: 35,
+  dogs: 70
+}
+
+const cartWheelScores = {
+  yes: 100,
+  no: 40
+}
+
+
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     // $("#char1, #char2, #char3, #char4, #char5").hide();
     const ageScore = parseInt($("input#age").val());
-    
+    const season = $("select#season").val();
+    const weekDay = $("select#weekDay").val();
+    const catsOrDogs = $("select#catsOrDogs").val();
+    const cartWheel = $("select#cartWheel").val();
+
+    let seasonScore = 0
+    let weekDayScore = 0
+    let catsOrDogsScore = 0
+    let cartWheelScore = 0
+
+    if (season === "autumn") {
+      seasonScore = seasonScores.autumn
+      console.log(seasonScore)
+    }
+
 
     event.preventDefault();
   });
