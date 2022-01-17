76cade93d3ae2f2761fda449a8b6b3aadfaf2f8e
jakechaley <jakechaley@gmail.com>
2022-01-16 18:40:47 -0800

    fix indentation/spelling error

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,7 @@ const seasonScores = {
   winter: 15,
   summer: 45,
   spring: 10
-}
-// console.log(seasonScores)
-// console.log(seasonScores.autumn)
+};
 
 const weekDayScores = {
   monday: 25,
@@ -15,23 +13,22 @@ const weekDayScores = {
   friday: 95,
   saturday: 50,
   sunday: 80  
-}
+};
 
 const catsOrDogsScores = {
   cats: 35,
   dogs: 70
-}
+};
 
 const cartWheelScores = {
   yes: 100,
   no: 40
-}
+};
 
 const language1 = 100;
 const language2 = 150;
 const language3 = 200;
 const language4 = 250;
-
 
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
@@ -42,50 +39,50 @@ $(document).ready(function() {
     const catsOrDogs = $("select#catsOrDogs").val();
     const cartWheel = $("select#cartWheel").val();
     
-    let seasonScore = 0
-    let weekDayScore = 0
-    let catsOrDogsScore = 0
-    let cartWheelScore = 0
+    let seasonScore = 0;
+    let weekDayScore = 0;
+    let catsOrDogsScore = 0;
+    let cartWheelScore = 0;
     
     if (season === "autumn") {
-      seasonScore = seasonScores.autumn
+      seasonScore = seasonScores.autumn;
     } else if (season === "winter") {
-      seasonScore = seasonScores.winter
+      seasonScore = seasonScores.winter;
     } else if (season === "spring") {
-      seasonScore = seasonScores.spring
+      seasonScore = seasonScores.spring;
     } else if (season === "summer") {
-      seasonScore = seasonScores.summer
+      seasonScore = seasonScores.summer;
     }
     
     if (weekDay === "monday") {
-      weekDayScore = weekDayScores.monday
+      weekDayScore = weekDayScores.monday;
     } else if (weekDay === "tuesday") {
-      weekDayScore = weekDayScores.tuesday
+      weekDayScore = weekDayScores.tuesday;
     } else if (weekDay === "wednesday") {
-      weekDayScore = weekDayScores.wednesday
+      weekDayScore = weekDayScores.wednesday;
     } else if (weekDay === "thursday") {
-      weekDayScore = weekDayScores.thursday
-    }else if (weekDay === "friday") {
-      weekDayScore = weekDayScores.friday
-    }else if (weekDay === "saturday") {
-      weekDayScore = weekDayScores.saturday
+      weekDayScore = weekDayScores.thursday;
+    } else if (weekDay === "friday") {
+      weekDayScore = weekDayScores.friday;
+    } else if (weekDay === "saturday") {
+      weekDayScore = weekDayScores.saturday;
     } else if (weekDay === "sunday") {
-      weekDayScore = weekDayScores.sunday
+      weekDayScore = weekDayScores.sunday;
     }
     
     if (catsOrDogs === "cats") {
-      catsOrDogsScore = catsOrDogsScores.cats
+      catsOrDogsScore = catsOrDogsScores.cats;
     } else if (catsOrDogs === "dogs") {
-      catsOrDogsScore = catsOrDogsScores.dogs
+      catsOrDogsScore = catsOrDogsScores.dogs;
     }
     
     if (cartWheel === "yes") {
-      cartWheelScore = cartWheelScores.yes
+      cartWheelScore = cartWheelScores.yes;
     } else if (cartWheel === "no") {
-      cartWheelScore = cartWheelScores.no
+      cartWheelScore = cartWheelScores.no;
     }
     
-    let finalScore = (ageScore + seasonScore + weekDayScore + catsOrDogsScore + cartWheelScore)
+    let finalScore = (ageScore + seasonScore + weekDayScore + catsOrDogsScore + cartWheelScore);
     
     if (finalScore <= language1) {
       $("#language1").show();
@@ -99,10 +96,7 @@ $(document).ready(function() {
       $("#language5").show();
     }
     
-    
-    
-    
+    $("#result-header").show();
     event.preventDefault();
-    console.log(finalScore)
   });
 });
